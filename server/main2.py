19286567ac7d609fc1347fc55b29836aaeae8c84
Egor Petr <sportasd@gmail.com>
2021-04-21 18:30:26 +0700

    ver 02

--- server/main2.py
+++ server/main2.py
@@ -1,16 +1,11 @@
-import socket
 import time
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-HOST_NAME = '127.0.0.1' # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = 53210 # Maybe set this to 9000.
+HOST_NAME = '127.0.0.1'
+PORT_NUMBER = 53210
 
 
 class MyHandler(BaseHTTPRequestHandler):
-    # def do_HEAD(s):
-    #     s.send_response(200)
-    #     s.send_header("Content-type", "text/html")
-    #     s.end_headers()
 
     def do_GET(s):
         """Respond to a GET request."""
@@ -21,8 +16,6 @@ class MyHandler(BaseHTTPRequestHandler):
             s.wfile.write(b"Pong")
         else:
             s.wfile.write(b"Please send ping.")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
 
 
 if __name__ == '__main__':
